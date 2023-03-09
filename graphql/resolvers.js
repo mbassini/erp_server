87a51a3f7a9ac4e8511ec7558a402255f67fae9c
Mauro Bassini <80562403+mbassini@users.noreply.github.com>
2023-03-09 08:05:48 -0300

    Add files via upload

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,59 +1,95 @@
-const { AuthenticationError } = require('apollo-server-express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const { AuthenticationError } = require("apollo-server-express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-const { Item, User } = require('../database/models');
+const { Item, User } = require("../database/models");
+
+const hashPassword = async password => bcrypt.hash(password, 10);
 
 module.exports = {
-	Mutation: {
-		async registerUser(root, { name, email, password }, _) {
-			const user = await User.findOne({ where: { email } });
+  Query: {
+    async user(_, { id }, ctx) {
+      return User.findByPk(id);
+    },
+    async users(_, { id }, ctx) {
+      return User.findAll();
+    },
+    async item(_, { id }, context) {
+      //TODO: Validations
+      return Item.findByPk(id);
+    },
+    async items(root, args, context) {
+      //TODO: Validations
+      return Item.findAll();
+    },
+  },
+  Mutation: {
+    async registerUser(root, { name, email, password }, _) {
+      const user = await User.findOne({ where: { email } });
 
-			if (user) throw new Error('Email already taken');
+      if (user) throw new Error("Email already taken");
 
-			const securePassword = bcrypt.hash(password, 10);
+      const securePassword = await hashPassword(password);
 
-			return User.create({ name, email, password: securePassword });
-		},
+      return User.create({ name, email, password: securePassword });
+    },
 
-		async loginUser(root, { email, password }, _) {
-			const user = await User.findOne({ where: { email } });
+    async updateUser(_, args, ctx) {
+      const updates = Object.keys(args);
+      const allowedUpdates = ["name", "email", "password"];
+      const isValidOperation = updates.every(update =>
+        allowedUpdates.includes(update)
+      );
 
-			if (!user) throw new Error('User does not exist!');
+      if (!isValidOperation) {
+        return { status: 403, success: false, message: "Cannot update field" };
+      }
 
-			if (bcrypt.compareSync(password, user.password)) {
-				const token = jwt.sign(
-					{ id: user.id },
-					process.env.JWT_SECRET,
-					{ expiresIn: '1h' }
-				);
-				return { ...user.toJSON(), token };
-			}
+      try {
+        const { name, email, password } = args;
+        const user = await User.findByPk(ctx.user.id);
+        await user.update({
+          name: name || user.name,
+          email: email || user.email,
+          password: password ? await hashPassword(password) : user.password,
+          updatedAt: new Date(),
+        });
+        return {
+          status: 204,
+          success: true,
+          message: "User successfully updated!",
+        };
+      } catch (e) {
+        console.error(e);
+        return { status: 500, success: false, message: e.message };
+      }
+    },
 
-			throw new AuthenticationError('Invalid credentials.');
-		},
+    async loginUser(root, { email, password }, _) {
+      const user = await User.findOne({ where: { email } });
 
-		async createItem(root, { name, quantity }, { user }) {
-			if (!user) throw new AuthenticationError('Please authenticate!');
+      if (!user) throw new Error("User does not exist!");
 
-			return Item.create({
-				name,
-				quantity: quantity ?? 0,
-				createdBy: user.id,
-				updatedBy: user.id,
-			});
-		},
-	},
+      if (bcrypt.compareSync(password, user.password)) {
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+          expiresIn: "1d",
+        });
+        return { ...user.toJSON(), token };
+      }
 
-	Query: {
-		async item(_, { id }, context) {
-			//TODO: Validations
-			return Item.findByPk(id);
-		},
-		async items(root, args, context) {
-			//TODO: Validations
-			return Item.findAll();
-		},
-	},
+      throw new AuthenticationError("Invalid credentials.");
+    },
+
+    async createItem(root, { name, quantity }, { user }) {
+      if (!user) throw new AuthenticationError("Please authenticate!");
+
+      return Item.create({
+        name,
+        quantity: quantity ?? 0,
+        createdBy: user.id,
+        updatedBy: user.id,
+      });
+    },
+  },
 };
--- graphql/schema.js
+++ graphql/schema.js
@@ -1,62 +1,76 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-	scalar DateTime
+  scalar DateTime
 
-	type User {
-		id: Int!
-		name: String!
-		email: String!
-		password: String!
-		createdAt: DateTime
-		updatedAt: DateTime
-	}
+  type User {
+    id: Int!
+    name: String!
+    email: String!
+    password: String!
+    createdAt: DateTime
+    updatedAt: DateTime
+  }
 
-	type RegisterResponse {
-		id: Int!
-		name: String!
-		email: String!
-	}
+  type RegisterResponse {
+    id: Int!
+    name: String!
+    email: String!
+  }
 
-	type LoginResponse {
-		id: Int!
-		name: String!
-		email: String!
-		token: String!
-	}
+  type LoginResponse {
+    id: Int!
+    name: String!
+    email: String!
+    token: String!
+  }
 
-	type Item {
-		id: Int!
-		name: String!
-		quantity: Int!
-		createdAt: DateTime
-		updatedAt: DateTime
-		createdBy: User!
-		updatedBy: User!
-	}
+  type Item {
+    id: Int!
+    name: String!
+    quantity: Int!
+    createdAt: DateTime
+    updatedAt: DateTime
+    createdBy: User!
+    updatedBy: User!
+  }
 
-	type CreateItemResponse {
-		id: Int!
-		name: String!
-		quantity: Int
-		createdAt: DateTime!
-	}
+  type CreateItemResponse {
+    id: Int!
+    name: String!
+    quantity: Int
+    createdAt: DateTime!
+  }
 
-	type Query {
-		item(id: Int!): Item!
-		items: [Item!]
-	}
+  type GenericMutationResult {
+    status: Int!
+    success: Boolean!
+    message: String!
+  }
 
-	type Mutation {
-		registerUser(
-			name: String!
-			email: String!
-			password: String!
-		): RegisterResponse
-		loginUser(email: String!, password: String!): LoginResponse
+  type Query {
+    user(id: Int!): User!
+    users: [User!]
 
-		createItem(name: String!, quantity: Int): CreateItemResponse
-	}
+    item(id: Int!): Item!
+    items: [Item!]
+  }
+
+  type Mutation {
+    registerUser(
+      name: String!
+      email: String!
+      password: String!
+    ): RegisterResponse
+    loginUser(email: String!, password: String!): LoginResponse
+    updateUser(
+      name: String
+      email: String
+      password: String
+    ): GenericMutationResult
+
+    createItem(name: String!, quantity: Int): CreateItemResponse
+  }
 `;
 
 module.exports = typeDefs;
